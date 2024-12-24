7e38e1f5249d05225716b56edfa122dfc28b66fc
yanchen <chen_4524@qq.com>
2024-12-24 22:23:13 +0800

    feat: impl the get , put and delete apis for LSM , only in in-mem version

--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -279,7 +279,7 @@ impl LsmStorageInner {
 
     /// Get a key from the storage. In day 7, this can be further optimized by using a bloom filter.
     pub fn get(&self, _key: &[u8]) -> Result<Option<Bytes>> {
-        unimplemented!()
+        Ok( self.state.read().memtable.get(_key) )
     }
 
     /// Write a batch of data into the storage. Implement in week 2 day 7.
@@ -289,12 +289,15 @@ impl LsmStorageInner {
 
     /// Put a key-value pair into the storage by writing into the current memtable.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        // as the memtable implementation only requires an immutable reference
+        // we only need take the read lock on state in order to modify the memtable
+       self.state.read().memtable.put(_key, _value)
     }
 
     /// Remove a key from the storage by writing an empty value.
     pub fn delete(&self, _key: &[u8]) -> Result<()> {
-        unimplemented!()
+        // the same reason as put for only getting the read lock
+        self.state.read().memtable.put(_key, &[])
     }
 
     pub(crate) fn path_of_sst_static(path: impl AsRef<Path>, id: usize) -> PathBuf {
